ddc7c899497707eeb0ba5d5fb78fd091359d156c
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2023-10-03 10:05:27 +0200

    Finalizado

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,12 @@ let numero = parseFloat(prompt("Por favor, ingresa un número:"));
 
 // Verificar si el número es positivo, negativo o igual a cero
 if (numero > 0) {
-  console.log("El número ingresado es positivo.");
+  console.log("El número ingresado es positivo.")
+  alert("El número ingresado es positivo.")
 } else if (numero < 0) {
-  console.log("El número ingresado es negativo.");
+  console.log("El número ingresado es negativo.")
+  alert("El número ingresado es negativo.")
 } else {
-  console.log("El número ingresado es igual a cero.");
+  console.log("El número ingresado es igual a cero.")
+  alert("El número ingresado es igual a cero.")
 }
